2a13f50f2291635c1d1e65d26dae99d8e0dae885
Ola2024Mohammed <hawaiiict2022@gmail.com>
2024-08-08 00:54:01 +0300

    Refactor PopupMenu .

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 List<String> showFilmsOptions = [
   "Most Popular ",
   "Highest Rated ",
-  "Favourites ",
+  "Trending ",
 ];
 
 const kPrimaryColor = Color.fromARGB(255, 2, 10, 46);
